c05fdf169c5058d1847c088ef7946b53a0314175
aahreum <cocoding420@gmail.com>
2026-02-25 22:51:48 +0900

    ⚙️ Chore: base 린트 설정 분리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'eslint/config';
-import js from '@eslint/js';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import importPlugin from 'eslint-plugin-import';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { baseConfig } from './eslint/base.mjs';
 
 export default defineConfig([
   {
@@ -17,75 +12,5 @@ export default defineConfig([
       '**/node_modules/**',
     ],
   },
-  js.configs.recommended,
-  eslintConfigPrettier,
-  {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 'latest',
-      },
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      import: importPlugin,
-    },
-    settings: {
-      'import/resolver': {
-        typescript: true,
-      },
-    },
-    rules: {
-      /*  TypeScript */
-      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: 'variable',
-          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-        },
-        {
-          selector: 'function',
-          format: ['camelCase', 'PascalCase'],
-        },
-        {
-          selector: 'typeLike',
-          format: ['PascalCase'],
-        },
-      ],
-      /*  import */
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
-          'newlines-between': 'never',
-        },
-      ],
-      'import/no-unresolved': 'off',
-      /* style */
-      curly: ['error', 'all'],
-      'prefer-destructuring': [
-        'warn',
-        {
-          VariableDeclarator: {
-            object: true,
-            array: false,
-          },
-        },
-      ],
-
-      'no-var': 'error',
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-    },
-  },
+  ...baseConfig,
 ]);
---
+++ eslint/base.mjs
@@ -0,0 +1,88 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export const baseConfig = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+
+    rules: {
+      /*  TypeScript */
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      /** TODO: 리턴 타입 명시할지 말지 정하기 */
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+      ],
+      /*  import */
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'never',
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'import/no-duplicates': 'error',
+      'import/newline-after-import': 'warn',
+      /* style */
+      curly: ['error', 'all'],
+      'prefer-destructuring': [
+        'warn',
+        {
+          VariableDeclarator: {
+            object: true,
+            array: false,
+          },
+        },
+      ],
+      'no-var': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  },
+];
