fac59e96554d4eb3dcc51f481c861a10c7bedd57
aahreum <cocoding420@gmail.com>
2026-02-27 22:36:56 +0900

    ⚙️ Chore: 스토리북 테스트 플러그인 추가

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -19,6 +19,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-vitest'),
   ],
   framework: getAbsolutePath('@storybook/react-vite'),
 };
